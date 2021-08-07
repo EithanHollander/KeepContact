7c23c3ed5b7f5eb72f026562f1f4afa106734377
EithanHollander <eithanhollander@gmail.com>
2021-08-07 07:14:50 +0300

    Add server side with dummy get method into AddConnection component

--- client/src/components/AddConnection/AddConnection.js
+++ client/src/components/AddConnection/AddConnection.js
@@ -1,11 +1,22 @@
 import './AddConnection.css';
 import AddIcon from '@material-ui/icons/Add';
+import React from 'react';
+
 
 function AddConnection () {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    console.log("WTF");
+    fetch("http://localhost:3001/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="AddConnection">
       <div className="add-connection-symbol"><span>+</span></div>
-      <div className="add-connection-text">Add Connection</div>
+      <div className="add-connection-text">{!data ? "Loading..." : data}</div>
     </div>
   );
 }
